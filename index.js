b5c590e219acceb1fbc55224dca8e035738f9a09
nasimbillah <nasimbillah269@gmail.com>
2022-11-08 07:49:00 +0600

    added database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,23 @@ const port = process.env.PORT || 5000;
 //midlwere
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.20te8rd.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
     res.send('special food service review server running')
